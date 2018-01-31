31abe7a1be523510d1c34d895a71519eb996a9bc
Pio Raffaele Fina <pioraffaelefina@outlook.it>
2018-01-31 17:01:24 +0100

    fixed seeder due to database schema changes

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -7,6 +7,6 @@ $factory->define(App\Group::class, function (Faker $faker) {
         'name'=> $faker->unique()->sentence($nbWords = 6, $variableNbWords = true),
         'subscribers_count' => $faker->numberBetween($min = 1, $max = 500),
         'description' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
-        'picture' => "https://picsum.photos/200/300/?random"
+        'picture_path' => "https://picsum.photos/200/300/?random"
     ];
 });
--- database/factories/PublicationFactory.php
+++ database/factories/PublicationFactory.php
@@ -8,7 +8,8 @@ $factory->define(App\Publication::class, function (Faker $faker) {
         'title' => $faker->catchPhrase.','.' '.$faker->bs,
         'type' => $faker->randomElement($array = array ('journal','conference','editorship')),
         'venue' => $faker->streetAddress.','.' '.$faker->country,
-        'multimedia' => "https://picsum.photos/200/300/?random",
+        'year' => $faker->dateTime($max = 'now', $timezone = null),
+        'multimedia_path' => "https://picsum.photos/200/300/?random",
         'public' => $faker->numberBetween($min = 0, $max = 1),
         'created_at' => $faker->dateTime($max = 'now', $timezone = null),
         'updated_at' => $faker->dateTime($max = 'now', $timezone = null)
