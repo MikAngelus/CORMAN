16d0ef4d0d134c9d36ff6134ed4c818a8a49a5b7
frepard <francescopaolo.derrico@gmail.com>
2018-02-01 10:05:25 +0100

    aggiustamento header responsive

--- resources/views/Pages/Group/groupSingle.blade.php
+++ resources/views/Pages/Group/groupSingle.blade.php
@@ -1,14 +1,16 @@
-<div id="group-box" class="col-sm-4 col-md-6 col-lg-4">
+<div id="group-box" class="col-12 col-sm-12 col-md-6 col-lg-4">
     <div class="panel panel-primary">
         <div class="panel-heading">
-            <i class="fa fa-users fa-2x" aria-hidden="true"></i>
-            <h2>{{$gro->name}}</h2>
+            <i class="icon fa fa-users fa-2x p-3 col-lg-2 col-md-2 col-sm" aria-hidden="true"></i>
+            <div class="mt-3 title col-lg-10 col-md-8 col-sm-8">
+                <h3 class="pl-2">{{$gro->name}}</h3>
+            </div>
         </div>
-        <div class="panel-body">{{$gro->description}}</div>
-        <div class="panel-footer">
+        <div class="panel-body p-3 text-justify">{{$gro->description}}</div>
+        <div class="panel-footer col-lg-12">
             <i class="fa fa-eye fa-2x" href="#"></i>
             <i class="fa fa-pencil fa-2x" href="#"></i>
-            <button class="btn-primary pull-right">View More</button>
+            <button class="btn btn-sm btn-primary pull-right">View More</button>
         </div>
     </div>
 </div>
