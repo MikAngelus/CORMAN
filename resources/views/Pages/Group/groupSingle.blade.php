73276cf793097d5ca4a93c07a650ae6bbf200663
frepard <francescopaolo.derrico@gmail.com>
2018-02-01 10:09:33 +0100

    sistemazione groupSingle

--- resources/views/Pages/Group/groupSingle.blade.php
+++ resources/views/Pages/Group/groupSingle.blade.php
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-<div id="group-box" class="col-12 col-sm-12 col-md-6 col-lg-4">
-    <div class="panel panel-primary">
-        <div class="panel-heading">
-            <i class="icon fa fa-users fa-2x p-3 col-lg-2 col-md-2 col-sm" aria-hidden="true"></i>
-            <div class="mt-3 title col-lg-10 col-md-8 col-sm-8">
-                <h3 class="pl-2">{{$gro->name}}</h3>
-            </div>
-        </div>
-        <div class="panel-body p-3 text-justify">{{$gro->description}}</div>
-        <div class="panel-footer col-lg-12">
-            <i class="fa fa-eye fa-2x" href="#"></i>
-            <i class="fa fa-pencil fa-2x" href="#"></i>
-            <button class="btn btn-sm btn-primary pull-right">View More</button>
-        </div>
-=======
 <div class="panel panel-primary col-lg-12 col-md-12">
     <div class="panel-heading">
         <i class="fa fa-users fa-2x" aria-hidden="true"></i>
@@ -24,7 +8,6 @@
         <i class="fa fa-eye fa-2x" href="#"></i>
         <i class="fa fa-pencil fa-2x" href="#"></i>
         <button class="btn-primary pull-right">View More</button>
->>>>>>> origin/master
     </div>
 </div>
 
