07656865d9e541d2dc5d8b58aa67be198fe4907f
sisa95ita <sabatosimone@gmail.com>
2018-02-05 13:05:19 +0100

    Fixed path to upload file in RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -112,7 +112,7 @@ class RegisterController extends Controller
                 
                 $hashName =  "/".md5($file->path().date('c'));
                 $fileName = $hashName . "." . $file->getClientOriginalExtension();
-                $filePath = public_path('images/profilePictures') . $fileName;
+                $filePath = 'images/profilePictures' . $fileName;
                 Image::make($file)->fit(200)->save($filePath);
                 $newUser->picture_path = $fileName;
             }
