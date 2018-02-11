8e1d4532c490e46d50187c889e6afca2c13c9a59
sisa95ita <sabatosimone@gmail.com>
2018-02-11 18:27:22 +0100

    Edited header file

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -13,17 +13,18 @@ class NotificationController extends Controller
         if(strlen($id)==36){
             $acceptedNotification = $user->notifications()->where('id', $id)->first();
             Group::find($acceptedNotification->data['group']['id'])->users()->syncWithoutDetaching([$acceptedNotification->data['user']['id'] => ['state' => 'accepted']]);
-            //  $acceptedNotification->delete();
+             $acceptedNotification->delete();
             return redirect()->route('groups.show', $acceptedNotification->data['group']['id']);
         }else if(strlen($id)==37){
             $refusedid = substr($id, 0, strlen($id)-1);
             $refusedNotification = $user->notifications()->where('id', $refusedid)->first();
-            //$refusedNotification->delete();
+            $refusedNotification->delete();
             return redirect()->route('users.index');
         }else if(strlen($id)==38){
             $publicationid = substr($id, 0, strlen($id)-2);
             $publicationNotification = $user->notifications()->where('id', $publicationid)->first();
-            //$publicationNotification->delete();
+            $publicationNotification->delete();
+            
             return redirect()->route('publications.show', $publicationNotification->data['publication']['id']);
         }else{
             return redirect()->back();
