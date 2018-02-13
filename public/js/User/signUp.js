dc883d82722c95a975b01f2016ae626bb5e58b63
Pio Raffaele Fina <pioraffaelefina@outlook.it>
2018-02-13 14:24:38 +0100

    refiend signup, added * to required field

--- public/js/User/signUp.js
+++ public/js/User/signUp.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     // Initialize selct2 components
     $('#affiliationDropdown').select2({
-        placeholder: "Type your affiliation",
+        placeholder: "Type your affiliation*",
         tags:true
     });
 
     $('#topicsDropdown').select2({
-        placeholder: "Type your research area",
+        placeholder: "Type your research area*",
         maximumSelectionLength: 5,
         tags:true
     });
